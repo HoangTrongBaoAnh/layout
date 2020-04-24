2739d431118a278b9ecc9550b807060e4368e306
vinhtq2020 <quangvinh2015vi@gmail.com>
2020-04-24 18:47:15 +0700

    chinh sua danh muc

--- layout-danhmuc/danhmuc-javascripts.js
+++ layout-danhmuc/danhmuc-javascripts.js
@@ -1,2 +1,11 @@
-
-
+$(document).ready(function(){
+    $('.news-ticker').marquee({
+        duration: 10000,
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: true
+    });   
+    $('.menu-demuc').click(function(){
+        $(this).next().slideToggle(500);
+    })
+})
