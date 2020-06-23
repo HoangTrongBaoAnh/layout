02841b92e323bbca2a6a675bed5b43af4c76565c
HoangAnh <biminhiabo2@gmail.com>
2020-06-23 19:43:47 +0700

    display cart customers have purchased in userinfo and set timezone to vietnam timezone

--- themhang.php
+++ themhang.php
@@ -4,9 +4,11 @@ session_start();
 include_once('lib/DataProvider.php');
 include_once('lib/quantri.php');
 $iduser = $_SESSION['iduser'];
-$date = date("Y/m/d");
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+$date = date("Y/m/d H:i:sa");
 $key= $_POST['key'];
 $idgame =  $_POST['idgame'];
 $qr = "INSERT INTO `hoadon` (`idhoadon`, `code`, `idgame`, `iduser`, `ngay`) VALUES (NULL,'$key', '$idgame', '$iduser', '$date');";
 DataProvider::ExecuteQuery($qr);
+//header("location: ./gameDetail.php?idgame="+$idgame);
 ?>
